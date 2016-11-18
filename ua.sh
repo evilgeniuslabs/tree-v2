e2c2848dee20adec1dcaced0f0bb65a5ba5c07c9
Mark Belles <mark.belles@firemon.com>
2016-11-18 15:34:01 -0600

    Bug fixes for the app upload script

--- ua.sh
+++ ua.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 # upload the web app to the board
 
-url=${1:-""}
+url=${1:-"http://requestb.in/1lj873x1"}
 gzip -rkf data
 
-echo ""
-echo data/css/bootstrap.min.css.gz
-curl -v --trace-ascii curl.log -F "filename='/css/bootstrap.min.css'" -F "file=@data/css/bootstrap.min.css.gz" http://192.168.1.14/edit
+# add --trace-ascii curl.log for logging
+curl -v --form "file=@data/css/bootstrap.min.css.gz;filename=css/bootstrap.min.css" $url
+curl -v --form "file=@data/css/styles.css.gz;filename=css/styles.css" $url
+curl -v --form "file=@data/js/app.js.gz;filename=js/app.js" $url
+curl -v --form "file=@data/index.htm.gz;filename=index.htm" $url
+curl -v --form "file=@data/images/atom196.png.gz;filename=images/atom196.png" $url
+curl -v --form "file=@data/favicon.ico.gz;filename=favicon.ico" $url
 
-#echo ""
-#echo data/index.htm.gz
-#curl -v -F "file=@data/index.htm.gz" $url
+# remove all of the gz files
 find . -name "*.gz" -type f -delete
